c99573b2119dd70c7bc153e8857d90ff17e7a75c
Sahil Kumar <sahilkumar012004@gmail.com>
2024-09-01 17:46:16 +0530

    Added all files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-3xl bg-orange-400">hello bro</h1>
+    </>
+  );
+}
+
+export default App;
